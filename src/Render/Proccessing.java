1eb4d03d76633679609c800dab79fd046b1a9b42
Theodore <theodorejsackos@email.arizona.edu>
2016-05-04 23:57:19 -0700

    Updated Processing

--- src/Render/Proccessing.java
+++ src/Render/Proccessing.java
@@ -13,47 +13,43 @@ import org.opencv.core.Mat;
 public class Proccessing {
 
 	/**
-	 * This code was written by medloh
-	 * (http://stackoverflow.com/users/2917835/medloh) and was taken from
 	 * http:// stackoverflow.com/questions/30262834/convert-image-in-mat-to-
 	 * bufferedimage
 	 */
 	public static BufferedImage matToBufferedImage(Mat matrix,
 			BufferedImage bimg) {
-		if (matrix != null) {
-			int cols = matrix.cols();
-			int rows = matrix.rows();
-			int elemSize = (int) matrix.elemSize();
-			byte[] data = new byte[cols * rows * elemSize];
-			int type;
-			matrix.get(0, 0, data);
-			switch (matrix.channels()) {
-			case 1:
-				type = BufferedImage.TYPE_BYTE_GRAY;
-				break;
-			case 3:
-				type = BufferedImage.TYPE_3BYTE_BGR;
-				// bgr to rgb
-				byte b;
-				for (int i = 0; i < data.length; i = i + 3) {
-					b = data[i];
-					data[i] = data[i + 2];
-					data[i + 2] = b;
-				}
-				break;
-			default:
-				return null;
-			}
+		if (matrix == null)
+			return null;
 
-			// Reuse existing BufferedImage if possible
-			if (bimg == null || bimg.getWidth() != cols
-					|| bimg.getHeight() != rows || bimg.getType() != type) {
-				bimg = new BufferedImage(cols, rows, type);
+		int cols = matrix.cols();
+		int rows = matrix.rows();
+		int elemSize = (int) matrix.elemSize();
+		byte[] raw = new byte[cols * rows * elemSize];
+		int imageType;
+		matrix.get(0, 0, raw);
+
+		int val = matrix.channels();
+		if (val == 1)
+			imageType = BufferedImage.TYPE_BYTE_GRAY;
+		else if (val == 3) {
+			imageType = BufferedImage.TYPE_3BYTE_BGR;
+			// bgr to rgb
+			byte b;
+			for (int i = 0; i < raw.length; i = i + 3) {
+				b = raw[i];
+				raw[i] = raw[i + 2];
+				raw[i + 2] = b;
 			}
-			bimg.getRaster().setDataElements(0, 0, cols, rows, data);
-		} else { // mat was null
-			bimg = null;
+		} else
+			return null;
+
+		// Reuse existing BufferedImage if possible
+		if (bimg == null || bimg.getWidth() != cols || bimg.getHeight() != rows
+				|| bimg.getType() != imageType) {
+			bimg = new BufferedImage(cols, rows, imageType);
 		}
+		bimg.getRaster().setDataElements(0, 0, cols, rows, raw);
+
 		return bimg;
 	}
 }
